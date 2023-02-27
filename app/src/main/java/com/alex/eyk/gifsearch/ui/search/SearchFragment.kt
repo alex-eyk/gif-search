22f85398e8731e9044b15b54b7204a485cf21587
Alexey Kiselev <ximand931@gmail.com>
2023-02-27 21:53:10 +0300

    SearchFragment fix (wrong generic)

--- app/src/main/java/com/alex/eyk/gifsearch/ui/search/SearchFragment.kt
+++ app/src/main/java/com/alex/eyk/gifsearch/ui/search/SearchFragment.kt
@@ -2,12 +2,12 @@ package com.alex.eyk.gifsearch.ui.search
 
 import androidx.fragment.app.viewModels
 import com.alex.eyk.gifsearch.R
-import com.alex.eyk.gifsearch.databinding.FragmentGifInfoBinding
+import com.alex.eyk.gifsearch.databinding.FragmentGifSearchBinding
 import com.alex.eyk.gifsearch.ui.AbstractFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SearchFragment : AbstractFragment<FragmentGifInfoBinding>(
+class SearchFragment : AbstractFragment<FragmentGifSearchBinding>(
     layoutRes = R.layout.fragment_gif_search
 ) {
 
