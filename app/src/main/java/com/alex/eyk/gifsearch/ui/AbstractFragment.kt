2b976a7abab59687c60813b673635867a3f360e5
Alexey Kiselev <ximand931@gmail.com>
2023-02-28 23:43:36 +0300

    Collect states in AbstractFragment

--- app/src/main/java/com/alex/eyk/gifsearch/ui/AbstractFragment.kt
+++ app/src/main/java/com/alex/eyk/gifsearch/ui/AbstractFragment.kt
@@ -8,6 +8,11 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 abstract class AbstractFragment<B : ViewDataBinding>(
     @LayoutRes private val layoutRes: Int
@@ -29,8 +34,20 @@ abstract class AbstractFragment<B : ViewDataBinding>(
             lifecycleOwner = viewLifecycleOwner
         }
         onBindingCreated()
+        collectStates()
         return binding.root
     }
 
+    private fun collectStates() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(
+                state = Lifecycle.State.STARTED,
+                block = onCollectStates()
+            )
+        }
+    }
+
     protected open fun onBindingCreated() {}
+
+    protected open fun onCollectStates(): suspend CoroutineScope.() -> Unit = {}
 }
