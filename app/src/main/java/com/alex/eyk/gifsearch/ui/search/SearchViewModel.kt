6b2a66ad832c4adef90ba9902e61d8937fb77820
Alexey Kiselev <ximand931@gmail.com>
2023-03-02 12:59:21 +0300

    Next results method

--- app/src/main/java/com/alex/eyk/gifsearch/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/alex/eyk/gifsearch/ui/search/SearchViewModel.kt
@@ -20,40 +20,70 @@ class SearchViewModel @Inject constructor(
     private val suggestionsRepository: SuggestionsRepository
 ) : ViewModel() {
 
+    companion object {
+
+        private const val GIFS_PER_REQUEST = 25
+    }
+
     private val _searchResults = MutableStateFlow<UiState<List<Gif>>>(UiState.None)
+    val searchResults: StateFlow<UiState<List<Gif>>> = _searchResults
+
+    private val _nextResults = MutableStateFlow<UiState<List<Gif>>>(UiState.None)
+    val nextResults: StateFlow<UiState<List<Gif>>> = _nextResults
+
     private val _suggestions = MutableStateFlow<UiState<List<Suggestion>>>(UiState.None)
+    val suggestions: StateFlow<UiState<List<Suggestion>>> = _suggestions
 
     val query = MutableStateFlow("")
 
-    val searchResults: StateFlow<UiState<List<Gif>>> = _searchResults
-    val suggestions: StateFlow<UiState<List<Suggestion>>> = _suggestions
-
-    fun search() {
-        if (query.value.isBlank()) {
-            if (_searchResults.value is Success) {
-                _searchResults.value = UiState.None
-            }
-        }
-        viewModelScope.launch {
-            _searchResults.value = UiState.by {
-                gifRepository.findAll(
-                    query.value,
-                    offset = 0
-                )
-            }
-        }
-    }
+    private var offset = 0
 
     fun onSuggestionSelected(
         suggestion: Suggestion
     ) {
         query.value = suggestion.name
-        search()
+        searchGifs()
+    }
+
+    private fun searchGifs() {
+        if (query.value.isBlank()) {
+            if (_searchResults.value is Success) {
+                _searchResults.value = UiState.None
+            }
+        }
+        _searchResults.value = UiState.Loading
+        viewModelScope.launch {
+            _searchResults.value = UiState.by {
+                gifRepository.findAll(
+                    query.value,
+                    limit = GIFS_PER_REQUEST,
+                    offset = 0
+                )
+            }
+            offset = GIFS_PER_REQUEST
+        }
+    }
+
+    fun searchNextGifs() {
+        if (query.value.isBlank()) {
+            return
+        }
+        _nextResults.value = UiState.Loading
+        viewModelScope.launch {
+            _nextResults.value = UiState.by {
+                gifRepository.findAll(
+                    query.value,
+                    limit = GIFS_PER_REQUEST,
+                    offset = offset
+                )
+            }
+            offset += GIFS_PER_REQUEST
+        }
     }
 
     fun updateSuggestions() {
         if (query.value.isBlank()) {
-            return
+            _suggestions.value = UiState.None
         }
         viewModelScope.launch {
             _suggestions.value = UiState.by {
