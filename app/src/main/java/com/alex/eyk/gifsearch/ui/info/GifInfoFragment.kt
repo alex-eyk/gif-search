a9ba2d8fade3670567a7c3d1d3e31a2de3ca76df
Alexey Kiselev <ximand931@gmail.com>
2023-03-04 15:09:59 +0300

    Gif info fragment

--- app/src/main/java/com/alex/eyk/gifsearch/ui/info/GifInfoFragment.kt
+++ app/src/main/java/com/alex/eyk/gifsearch/ui/info/GifInfoFragment.kt
@@ -1,11 +1,22 @@
 package com.alex.eyk.gifsearch.ui.info
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.util.DisplayMetrics
+import android.widget.LinearLayout
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.alex.eyk.gifsearch.R
 import com.alex.eyk.gifsearch.databinding.FragmentGifInfoBinding
 import com.alex.eyk.gifsearch.ui.AbstractFragment
+import com.alex.eyk.gifsearch.ui.info.GifInfoViewModel.Event
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.LENGTH_SHORT
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+
 
 @AndroidEntryPoint
 class GifInfoFragment : AbstractFragment<FragmentGifInfoBinding>(
@@ -15,4 +26,70 @@ class GifInfoFragment : AbstractFragment<FragmentGifInfoBinding>(
     private val viewModel by viewModels<GifInfoViewModel>()
 
     private val navArgs by navArgs<GifInfoFragmentArgs>()
+
+    override fun onBindingCreated() {
+        super.onBindingCreated()
+        viewModel.gif = navArgs.gif
+        binding.viewModel = viewModel
+        binding.executePendingBindings()
+        binding.gifImage.apply {
+            layoutParams = getGifImageLayoutParams()
+        }
+    }
+
+    override fun onCollectStates(): suspend CoroutineScope.() -> Unit = {
+        viewModel.eventsFlow.collect {
+            when (it) {
+                is Event.OpenUrl -> {
+                    openUrl(it.url)
+                }
+                is Event.NotifyCopied -> {
+                    notifyCopied()
+                }
+            }
+        }
+    }
+
+    private fun openUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(url)
+        }
+        startActivity(intent)
+    }
+
+    private fun notifyCopied() {
+        Snackbar.make(
+            requireView(),
+            R.string.link_copied,
+            LENGTH_SHORT
+        ).show()
+    }
+
+    private fun getGifImageLayoutParams(): LinearLayout.LayoutParams {
+        val image = navArgs.gif.images.original
+        return LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            calculateImageHeight(image.width, image.height)
+        )
+    }
+
+    private fun calculateImageHeight(
+        imageWidth: Int,
+        imageHeight: Int
+    ): Int {
+        val multiplier: Float = getScreenWidth() / imageWidth.toFloat()
+        return (imageHeight * multiplier).toInt()
+    }
+
+    private fun getScreenWidth(): Int {
+        val windowManager = requireActivity().windowManager
+        return if (VERSION.SDK_INT >= VERSION_CODES.R) {
+            windowManager.currentWindowMetrics.bounds.width()
+        } else {
+            val metrics = DisplayMetrics()
+            @Suppress("DEPRECATION")
+            windowManager.defaultDisplay.getMetrics(metrics)
+            metrics.widthPixels
+        }
+    }
 }
--- app/src/main/java/com/alex/eyk/gifsearch/ui/info/GifInfoViewModel.kt
+++ app/src/main/java/com/alex/eyk/gifsearch/ui/info/GifInfoViewModel.kt
@@ -1,8 +1,54 @@
 package com.alex.eyk.gifsearch.ui.info
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.alex.eyk.gifsearch.data.entity.Gif
+import com.alex.eyk.gifsearch.domain.clipboard.CopyUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class GifInfoViewModel @Inject constructor() : ViewModel()
+class GifInfoViewModel @Inject constructor(
+    private val copyUseCase: CopyUseCase
+) : ViewModel() {
+
+    sealed class Event {
+
+        data class OpenUrl(val url: String) : Event()
+
+        data class NotifyCopied(val url: String) : Event()
+    }
+
+    companion object {
+
+        private const val GIF_URL_LABEL = "gif url"
+    }
+
+    lateinit var gif: Gif
+
+    private val eventChannel = Channel<Event>(Channel.BUFFERED)
+    val eventsFlow = eventChannel.receiveAsFlow()
+
+    fun onUrlClick() {
+        viewModelScope.launch {
+            eventChannel.send(
+                Event.OpenUrl(gif.shortUrl)
+            )
+        }
+    }
+
+    fun onUrlLongClick() {
+        viewModelScope.launch {
+            copyUseCase.copy(
+                GIF_URL_LABEL,
+                data = gif.shortUrl
+            )
+            eventChannel.send(
+                Event.NotifyCopied(gif.shortUrl)
+            )
+        }
+    }
+}
